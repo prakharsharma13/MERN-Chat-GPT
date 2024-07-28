1c91daad7e366cc1e25f7f48b6bb2941cf7d43ca
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-07-29 01:40:50 +0530

    Animated homepage design

--- client/src/routes/homepage/Homepage.jsx
+++ client/src/routes/homepage/Homepage.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import "./homepage.css"
-import { Link } from 'react-router-dom';
+import "./homepage.css";
+import {Link} from "react-router-dom"
 
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <Link to="/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt="" className='orbital' />
+      <div className="left">
+        <h1>MERN-AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit aliquid minus amet illo nostrum</h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className='bot' />
+        </div>
+      </div>
     </div>
   )
 }
