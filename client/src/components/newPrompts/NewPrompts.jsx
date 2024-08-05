1b7349b5fd1e958b17203b31f7fb083b42b0ecfc
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-08-05 10:44:14 +0530

    AI chat using Gemini API

--- client/src/components/newPrompts/NewPrompts.jsx
+++ client/src/components/newPrompts/NewPrompts.jsx
@@ -1,42 +1,69 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import "./newPrompts.css";
-import Upload from '../upload/Upload';
-import { IKImage } from 'imagekitio-react';
+import Upload from "../upload/Upload";
+import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
+import Markdown from "react-markdown";
 
 const NewPrompts = () => {
-
-  const[img,setImg] = useState({
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [img, setImg] = useState({
     isLoading: false,
     error: "",
-    dbData : {}
-  })
+    dbData: {},
+  });
   const endRef = useRef(null);
 
   useEffect(() => {
-    endRef.current.scrollIntoView({behavior: "smooth"})
-  },[])
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [question, answer, img.dbData]);
+
+  const add = async (text) => {
+    setQuestion(text);
+
+    const result = await model.generateContent(text);
+    const response = await result.response;
+    setAnswer(response.text());
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const text = e.target.text.value;
+    if (!text) return;
+
+    add(text);
+  };
   return (
     <>
-    {img.isLoading && <div>Loading...</div>}
-    {img.dbData?.filePath && (
-      <IKImage
-        urlEndpoint= {import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
-        path={img.dbData?.filePath}
-        width="380"
-        transformation={[{width: "380"}]}
-      />
-    )}
-    <div className="endChat" ref={endRef}></div>
-        <form className="newForm">
-            <Upload setImg={setImg}/>
-            <input id='file' type="file" multiple={false} hidden/>
-            <input type="text" placeholder='Ask anything....' />
-            <button>
-                <img src="/arrow.png" alt="" />
-            </button>
-        </form>
-    </>
-  )
-}
+      {img.isLoading && <div>Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={[{ width: "380" }]}
+        />
+      )}
+      {question && <div className="message user">{question}</div>}
+      {answer && (
+        <div className="message">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
 
-export default NewPrompts
+      <div className="endChat" ref={endRef}></div>
+      <form className="newForm" onSubmit={handleSubmit}>
+        <Upload setImg={setImg} />
+        <input id="file" type="file" multiple={false} hidden />
+        <input type="text" name="text" placeholder="Ask anything...." />
+        <button>
+          <img src="/arrow.png" alt="" />
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default NewPrompts;
